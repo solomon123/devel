4c9a608ddc39ab830b2b23849924c78a87d3815f
solomon123 <solomonyif@gmail.com>
2017-01-30 18:17:48 -0500

    Initial push

---
+++ one/sites/default/files/js/js_3BPhccWajuaRlDyMMFeI07_E2nTaO8OZ5n88MDev5pc.js
@@ -0,0 +1,448 @@
+(function ($) {
+
+/**
+ * Attaches double-click behavior to toggle full path of Krumo elements.
+ */
+Drupal.behaviors.devel = {
+  attach: function (context, settings) {
+
+    // Add hint to footnote
+    $('.krumo-footnote .krumo-call').once().before('<img style="vertical-align: middle;" title="Click to expand. Double-click to show path." src="' + settings.basePath + 'misc/help.png"/>');
+
+    var krumo_name = [];
+    var krumo_type = [];
+
+    function krumo_traverse(el) {
+      krumo_name.push($(el).html());
+      krumo_type.push($(el).siblings('em').html().match(/\w*/)[0]);
+
+      if ($(el).closest('.krumo-nest').length > 0) {
+        krumo_traverse($(el).closest('.krumo-nest').prev().find('.krumo-name'));
+      }
+    }
+
+    $('.krumo-child > div:first-child', context).dblclick(
+      function(e) {
+        if ($(this).find('> .krumo-php-path').length > 0) {
+          // Remove path if shown.
+          $(this).find('> .krumo-php-path').remove();
+        }
+        else {
+          // Get elements.
+          krumo_traverse($(this).find('> a.krumo-name'));
+
+          // Create path.
+          var krumo_path_string = '';
+          for (var i = krumo_name.length - 1; i >= 0; --i) {
+            // Start element.
+            if ((krumo_name.length - 1) == i)
+              krumo_path_string += '$' + krumo_name[i];
+
+            if (typeof krumo_name[(i-1)] !== 'undefined') {
+              if (krumo_type[i] == 'Array') {
+                krumo_path_string += "[";
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += krumo_name[(i-1)];
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += "]";
+              }
+              if (krumo_type[i] == 'Object')
+                krumo_path_string += '->' + krumo_name[(i-1)];
+            }
+          }
+          $(this).append('<div class="krumo-php-path" style="font-family: Courier, monospace; font-weight: bold;">' + krumo_path_string + '</div>');
+
+          // Reset arrays.
+          krumo_name = [];
+          krumo_type = [];
+        }
+      }
+    );
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * A progressbar object. Initialized with the given id. Must be inserted into
+ * the DOM afterwards through progressBar.element.
+ *
+ * method is the function which will perform the HTTP request to get the
+ * progress bar state. Either "GET" or "POST".
+ *
+ * e.g. pb = new progressBar('myProgressBar');
+ *      some_element.appendChild(pb.element);
+ */
+Drupal.progressBar = function (id, updateCallback, method, errorCallback) {
+  var pb = this;
+  this.id = id;
+  this.method = method || 'GET';
+  this.updateCallback = updateCallback;
+  this.errorCallback = errorCallback;
+
+  // The WAI-ARIA setting aria-live="polite" will announce changes after users
+  // have completed their current activity and not interrupt the screen reader.
+  this.element = $('<div class="progress" aria-live="polite"></div>').attr('id', id);
+  this.element.html('<div class="bar"><div class="filled"></div></div>' +
+                    '<div class="percentage"></div>' +
+                    '<div class="message">&nbsp;</div>');
+};
+
+/**
+ * Set the percentage and status message for the progressbar.
+ */
+Drupal.progressBar.prototype.setProgress = function (percentage, message) {
+  if (percentage >= 0 && percentage <= 100) {
+    $('div.filled', this.element).css('width', percentage + '%');
+    $('div.percentage', this.element).html(percentage + '%');
+  }
+  $('div.message', this.element).html(message);
+  if (this.updateCallback) {
+    this.updateCallback(percentage, message, this);
+  }
+};
+
+/**
+ * Start monitoring progress via Ajax.
+ */
+Drupal.progressBar.prototype.startMonitoring = function (uri, delay) {
+  this.delay = delay;
+  this.uri = uri;
+  this.sendPing();
+};
+
+/**
+ * Stop monitoring progress via Ajax.
+ */
+Drupal.progressBar.prototype.stopMonitoring = function () {
+  clearTimeout(this.timer);
+  // This allows monitoring to be stopped from within the callback.
+  this.uri = null;
+};
+
+/**
+ * Request progress data from server.
+ */
+Drupal.progressBar.prototype.sendPing = function () {
+  if (this.timer) {
+    clearTimeout(this.timer);
+  }
+  if (this.uri) {
+    var pb = this;
+    // When doing a post request, you need non-null data. Otherwise a
+    // HTTP 411 or HTTP 406 (with Apache mod_security) error may result.
+    $.ajax({
+      type: this.method,
+      url: this.uri,
+      data: '',
+      dataType: 'json',
+      success: function (progress) {
+        // Display errors.
+        if (progress.status == 0) {
+          pb.displayError(progress.data);
+          return;
+        }
+        // Update display.
+        pb.setProgress(progress.percentage, progress.message);
+        // Schedule next timer.
+        pb.timer = setTimeout(function () { pb.sendPing(); }, pb.delay);
+      },
+      error: function (xmlhttp) {
+        pb.displayError(Drupal.ajaxError(xmlhttp, pb.uri));
+      }
+    });
+  }
+};
+
+/**
+ * Display errors on the page.
+ */
+Drupal.progressBar.prototype.displayError = function (string) {
+  var error = $('<div class="messages error"></div>').html(string);
+  $(this.element).before(error).hide();
+
+  if (this.errorCallback) {
+    this.errorCallback(this);
+  }
+};
+
+})(jQuery);
+;
+/**
+ * @file
+ *
+ * Fivestar AJAX for updating fivestar widgets.
+ */
+
+/**
+ * Create a degradeable star rating interface out of a simple form structure.
+ */
+(function($){ // Create local scope.
+
+Drupal.ajax.prototype.commands.fivestarUpdate = function (ajax, response, status) {
+  response.selector = $('.fivestar-form-item', ajax.element.form);
+  ajax.commands.insert(ajax, response, status);
+};
+
+})(jQuery);
+;
+/**
+ * @file
+ *
+ * Fivestar JavaScript behaviors integration.
+ */
+
+/**
+ * Create a degradeable star rating interface out of a simple form structure.
+ *
+ * Originally based on the Star Rating jQuery plugin by Wil Stuckey:
+ * http://sandbox.wilstuckey.com/jquery-ratings/
+ */
+(function($){ // Create local scope.
+
+Drupal.behaviors.fivestar = {
+  attach: function (context) {
+    $(context).find('div.fivestar-form-item').once('fivestar', function() {
+      var $this = $(this);
+      var $container = $('<div class="fivestar-widget clearfix"></div>');
+      var $select = $('select', $this);
+
+      // Setup the cancel button
+      var $cancel = $('option[value="0"]', $this);
+      if ($cancel.length) {
+        $('<div class="cancel"><a href="#0" title="' + $cancel.text() + '">' + $cancel.text() + '</a></div>')
+          .appendTo($container);
+      }
+
+      // Setup the rating buttons
+      var $options = $('option', $this).not('[value="-"], [value="0"]');
+      var index = -1;
+      $options.each(function(i, element) {
+        var classes = 'star-' + (i+1);
+        classes += (i + 1) % 2 == 0 ? ' even' : ' odd';
+        classes += i == 0 ? ' star-first' : '';
+        classes += i + 1 == $options.length ? ' star-last' : '';
+        $('<div class="star"><a href="#' + element.value + '" title="' + element.text + '">' + element.text + '</a></div>')
+          .addClass(classes)
+          .appendTo($container);
+        if (element.value == $select.val()) {
+          index = i + 1;
+        }
+      });
+
+      if (index != -1) {
+        $container.find('.star').slice(0, index).addClass('on');
+      }
+      $container.addClass('fivestar-widget-' + ($options.length));
+      $container.find('a')
+        .bind('click', $this, Drupal.behaviors.fivestar.rate)
+        .bind('mouseover', $this, Drupal.behaviors.fivestar.hover);
+
+      $container.bind('mouseover mouseout', $this, Drupal.behaviors.fivestar.hover);
+
+      // Attach the new widget and hide the existing widget.
+      $select.after($container).css('display', 'none');
+
+      // Allow other modules to modify the widget.
+      Drupal.attachBehaviors($this);
+    });
+  },
+  rate: function(event) {
+    var $this = $(this);
+    var $widget = event.data;
+    var value = this.hash.replace('#', '');
+    $('select', $widget).val(value).change();
+    var $this_star = (value == 0) ? $this.parent().parent().find('.star') : $this.closest('.star');
+    $this_star.prevAll('.star').andSelf().addClass('on');
+    $this_star.nextAll('.star').removeClass('on');
+    if(value==0){
+      $this_star.removeClass('on');
+    }
+
+    event.preventDefault();
+  },
+  hover: function(event) {
+    var $this = $(this);
+    var $widget = event.data;
+    var $target = $(event.target);
+    var $stars = $('.star', $this);
+
+    if (event.type == 'mouseover') {
+      var index = $stars.index($target.parent());
+      $stars.each(function(i, element) {
+        if (i <= index) {
+          $(element).addClass('hover');
+        } else {
+          $(element).removeClass('hover');
+        }
+      });
+    } else {
+      $stars.removeClass('hover');
+    }
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Automatically display the guidelines of the selected text format.
+ */
+Drupal.behaviors.filterGuidelines = {
+  attach: function (context) {
+    $('.filter-guidelines', context).once('filter-guidelines')
+      .find(':header').hide()
+      .closest('.filter-wrapper').find('select.filter-list')
+      .bind('change', function () {
+        $(this).closest('.filter-wrapper')
+          .find('.filter-guidelines-item').hide()
+          .siblings('.filter-guidelines-' + this.value).show();
+      })
+      .change();
+  }
+};
+
+})(jQuery);
+;
---
+++ one/sites/default/files/js/js_osWgP_jFSsCt6qS9saUALZs-m1vU-sFjl5rNF-RH7PY.js
@@ -0,0 +1,269 @@
+(function ($) {
+
+/**
+ * Attaches double-click behavior to toggle full path of Krumo elements.
+ */
+Drupal.behaviors.devel = {
+  attach: function (context, settings) {
+
+    // Add hint to footnote
+    $('.krumo-footnote .krumo-call').once().before('<img style="vertical-align: middle;" title="Click to expand. Double-click to show path." src="' + settings.basePath + 'misc/help.png"/>');
+
+    var krumo_name = [];
+    var krumo_type = [];
+
+    function krumo_traverse(el) {
+      krumo_name.push($(el).html());
+      krumo_type.push($(el).siblings('em').html().match(/\w*/)[0]);
+
+      if ($(el).closest('.krumo-nest').length > 0) {
+        krumo_traverse($(el).closest('.krumo-nest').prev().find('.krumo-name'));
+      }
+    }
+
+    $('.krumo-child > div:first-child', context).dblclick(
+      function(e) {
+        if ($(this).find('> .krumo-php-path').length > 0) {
+          // Remove path if shown.
+          $(this).find('> .krumo-php-path').remove();
+        }
+        else {
+          // Get elements.
+          krumo_traverse($(this).find('> a.krumo-name'));
+
+          // Create path.
+          var krumo_path_string = '';
+          for (var i = krumo_name.length - 1; i >= 0; --i) {
+            // Start element.
+            if ((krumo_name.length - 1) == i)
+              krumo_path_string += '$' + krumo_name[i];
+
+            if (typeof krumo_name[(i-1)] !== 'undefined') {
+              if (krumo_type[i] == 'Array') {
+                krumo_path_string += "[";
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += krumo_name[(i-1)];
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += "]";
+              }
+              if (krumo_type[i] == 'Object')
+                krumo_path_string += '->' + krumo_name[(i-1)];
+            }
+          }
+          $(this).append('<div class="krumo-php-path" style="font-family: Courier, monospace; font-weight: bold;">' + krumo_path_string + '</div>');
+
+          // Reset arrays.
+          krumo_name = [];
+          krumo_type = [];
+        }
+      }
+    );
+  }
+};
+
+})(jQuery);
+;
+/**
+ * @file
+ *
+ * Fivestar JavaScript behaviors integration.
+ */
+
+/**
+ * Create a degradeable star rating interface out of a simple form structure.
+ *
+ * Originally based on the Star Rating jQuery plugin by Wil Stuckey:
+ * http://sandbox.wilstuckey.com/jquery-ratings/
+ */
+(function($){ // Create local scope.
+
+Drupal.behaviors.fivestar = {
+  attach: function (context) {
+    $(context).find('div.fivestar-form-item').once('fivestar', function() {
+      var $this = $(this);
+      var $container = $('<div class="fivestar-widget clearfix"></div>');
+      var $select = $('select', $this);
+
+      // Setup the cancel button
+      var $cancel = $('option[value="0"]', $this);
+      if ($cancel.length) {
+        $('<div class="cancel"><a href="#0" title="' + $cancel.text() + '">' + $cancel.text() + '</a></div>')
+          .appendTo($container);
+      }
+
+      // Setup the rating buttons
+      var $options = $('option', $this).not('[value="-"], [value="0"]');
+      var index = -1;
+      $options.each(function(i, element) {
+        var classes = 'star-' + (i+1);
+        classes += (i + 1) % 2 == 0 ? ' even' : ' odd';
+        classes += i == 0 ? ' star-first' : '';
+        classes += i + 1 == $options.length ? ' star-last' : '';
+        $('<div class="star"><a href="#' + element.value + '" title="' + element.text + '">' + element.text + '</a></div>')
+          .addClass(classes)
+          .appendTo($container);
+        if (element.value == $select.val()) {
+          index = i + 1;
+        }
+      });
+
+      if (index != -1) {
+        $container.find('.star').slice(0, index).addClass('on');
+      }
+      $container.addClass('fivestar-widget-' + ($options.length));
+      $container.find('a')
+        .bind('click', $this, Drupal.behaviors.fivestar.rate)
+        .bind('mouseover', $this, Drupal.behaviors.fivestar.hover);
+
+      $container.bind('mouseover mouseout', $this, Drupal.behaviors.fivestar.hover);
+
+      // Attach the new widget and hide the existing widget.
+      $select.after($container).css('display', 'none');
+
+      // Allow other modules to modify the widget.
+      Drupal.attachBehaviors($this);
+    });
+  },
+  rate: function(event) {
+    var $this = $(this);
+    var $widget = event.data;
+    var value = this.hash.replace('#', '');
+    $('select', $widget).val(value).change();
+    var $this_star = (value == 0) ? $this.parent().parent().find('.star') : $this.closest('.star');
+    $this_star.prevAll('.star').andSelf().addClass('on');
+    $this_star.nextAll('.star').removeClass('on');
+    if(value==0){
+      $this_star.removeClass('on');
+    }
+
+    event.preventDefault();
+  },
+  hover: function(event) {
+    var $this = $(this);
+    var $widget = event.data;
+    var $target = $(event.target);
+    var $stars = $('.star', $this);
+
+    if (event.type == 'mouseover') {
+      var index = $stars.index($target.parent());
+      $stars.each(function(i, element) {
+        if (i <= index) {
+          $(element).addClass('hover');
+        } else {
+          $(element).removeClass('hover');
+        }
+      });
+    } else {
+      $stars.removeClass('hover');
+    }
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
